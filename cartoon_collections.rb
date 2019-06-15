706b61a431f6601c598ba45211983cf6508f9d09
Valena Del Valle <valenadelvalle4@gmail.com>
2019-06-15 23:33:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,30 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(names)
+
+names = [ 'Doc', 'Dopey', 'Bashful', 'Grumpy' ]
+ names.each do |name|
+  puts name
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+        #"1. #{names}"
+        #"2. #{names}"
+        #"3. #{names}"
+        # "4. #{names}"
+
+def summon_captain_planet(planeteer_calls)
+  
+calls = [ "Earth", "Wind", "Fire", "Water", "Heart"]
+  calls.collect { |x| x + "!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls_long)
+  
+  calls_long = ["axe", "earth", "wind", "fire"]
+  long_planeteer_calls(calls_long)
+  !!calls_long
+end
+  
+  
+   calls_short = ["wind", "fire", "tree", "axe", "code"]
 end
 
 def find_the_cheese# code an argument here
